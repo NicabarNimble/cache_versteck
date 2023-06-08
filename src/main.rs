3a0e727ff856fe8703c99d31ac3ef346aa39384f
NicabarNimble <nicabar@gmail.com>
2023-06-08 10:31:08 -0400

    back to basics d20

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use drand_core::HttpClient;
+use rand::{seq::SliceRandom, SeedableRng};
+use rand_chacha::ChaCha20Rng;
+
+#[tokio::main]
+async fn main() {
+    let client: HttpClient = "https://api.drand.sh".try_into().unwrap();
+    let latest = client.latest().await.unwrap();
+    let round = latest.round();
+
+    let seed: <ChaCha20Rng as SeedableRng>::Seed = latest.randomness().try_into().unwrap();
+    let mut rng = ChaCha20Rng::from_seed(seed);
+
+    let dice = vec![
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    ];
+
+    let roll = dice.choose(&mut rng).unwrap();
+    println!("Dice roll: {} (round {})", roll, round);
 }
